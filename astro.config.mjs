819cd26d22297a318ea9da2855490e3b23560b05
Lunadiving <lunadivingcenter@gmail.com>
2026-07-02 14:36:26 +0700

    Rimosso file dinamico che causava errori di build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro.luna-diving-thailand.com',
   vite: {
     plugins: [tailwindcss()]
   }
